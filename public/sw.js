45ea99ff621654916ea0b44cda3a720f97a1bb99
GonzaloDiaz1994 <gonzalodiaz1994@gmail.com>
2021-09-08 17:00:34 -0300

    Added caching strategies. Added offline page.

--- public/sw.js
+++ public/sw.js
@@ -13,12 +13,14 @@ self.addEventListener('install', function(event) {
           '/src/css/main.css',
           '/src/js/main.js',
           '/src/js/material.min.js',
+          '/offline.html',
           'https://fonts.googleapis.com/css?family=Roboto:400,700',
           'https://fonts.googleapis.com/icon?family=Material+Icons',
           'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
         ]);
       })
   )
+    self.skipWaiting()
 });
 
 self.addEventListener('activate', function(event) {
@@ -34,25 +36,53 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                });
-            })
-            .catch(function(err) {
+/** 1 - La estrategia utilizada fue: Cache with network fallback **/
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 });
+//             })
+//             .catch(function(err) {
+//
+//             });
+//         }
+//       })
+//   );
+// });
 
-            });
-        }
-      })
-  );
-});
+
+/** 2 - Estrategia: Network-only
+ *      Se necesita conexión. **/
+// self.addEventListener('fetch', (event) => {
+//    event.respondWith(fetch(event.request))
+// });
+
+/** 3 - Estrategia: Cache-only.
+ *      Si el usuario requiere un recurso que no está en la caché, ¿se muestra una página offline? **/
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(caches.match(event.request))
+// });
+
+/** 4 - Estrategia: Network, cache fallback. **/
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         fetch(event.request).then( (response) => {
+//             return caches.open(CACHE_DYNAMIC_NAME).then( (cache) => {
+//                 cache.put(event.request, response.clone())
+//                 return response;
+//             })
+//         }).catch( () => { return caches.match(event.request) })
+//     )
+// });
+
+/** 5 - Estrategia: Cache, then network **/
